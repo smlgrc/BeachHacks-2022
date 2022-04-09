6ac3b10713dc7efbcfb466fc1a9347662ba64044
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-04-09 15:46:04 -0700

    small updates

--- app/src/main/java/com/example/beachhacks2022/HangmanGame.java
+++ app/src/main/java/com/example/beachhacks2022/HangmanGame.java
@@ -10,5 +10,7 @@ public class HangmanGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman_game);
+
+        
     }
 }

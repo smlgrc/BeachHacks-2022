dc0e5302b734243ee56bdc8e87dd8fcef5b79441
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-04-09 23:57:15 -0700

    Added add boxes and add items

--- app/src/main/java/com/example/beachhacks2022/InsertBoxItems.java
+++ app/src/main/java/com/example/beachhacks2022/InsertBoxItems.java
@@ -2,14 +2,60 @@ package com.example.beachhacks2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class InsertBoxItems extends AppCompatActivity {
+    private LinearLayout layoutList2;
+    Button addItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_insert_items);
+        Box thisBox = new Box();
+
+//        ArrayList<Box> boxes = getIntent().getParcelableExtra("boxes");
+
+
+
+        layoutList2 = findViewById(R.id.layout_list2);
+
+        addItem = findViewById(R.id.button_add_new_item);
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View itemView = getLayoutInflater().inflate(R.layout.new_item, null,false);
+                ImageView imageClose = (ImageView)itemView.findViewById(R.id.row_remove2);
+                layoutList2.addView(itemView);
+//                boxes.add(new Box());
+
+
+                imageClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        layoutList2.removeView(itemView);
+                    }
+                });
+
+            }
+        });
+
+        Button goBackButton = findViewById(R.id.button_go_back);
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InsertBoxItems.this, InventoryScreen.class);
+                intent.putExtra("Box", thisBox);
+                startActivity(intent);
+            }
+        });
+
+
 
 
 
--- app/src/main/java/com/example/beachhacks2022/InventoryScreen.java
+++ app/src/main/java/com/example/beachhacks2022/InventoryScreen.java
@@ -2,19 +2,24 @@ package com.example.beachhacks2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 public class InventoryScreen extends AppCompatActivity {
     private LinearLayout layoutList;
+    private ArrayList<Box> boxes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_screen);
+
 
         layoutList = findViewById(R.id.layout_list);
 
@@ -25,6 +30,17 @@ public class InventoryScreen extends AppCompatActivity {
                 View boxView = getLayoutInflater().inflate(R.layout.add_box, null,false);
                 ImageView imageClose = (ImageView)boxView.findViewById(R.id.row_remove);
                 layoutList.addView(boxView);
+//                boxes.add(new Box());
+
+                Button thisButton = boxView.findViewById(R.id.button_box);
+                thisButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(InventoryScreen.this, InsertBoxItems.class);
+                        intent.putExtra("Boxes", boxes);
+                        startActivity(intent);
+                    }
+                });
 
                 imageClose.setOnClickListener(new View.OnClickListener() {
                     @Override
